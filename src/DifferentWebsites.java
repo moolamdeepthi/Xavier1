3074ffe88772825e9f50918a85fb35b039154232
deepthi.moolam <deepthi.moolam@D469.blr.velankani.com>
2016-03-04 12:10:29 +0530

    Searching for SDNbooks

--- src/DifferentWebsites.java
+++ src/DifferentWebsites.java
@@ -1,18 +1,15 @@
 
 	import java.awt.AWTException;
 	import java.awt.Robot;
-	import java.awt.Window;
 	import java.awt.event.KeyEvent;
 	import java.io.File;
 	import java.io.IOException;
-	import java.util.concurrent.TimeUnit;
-
 	import org.apache.commons.io.FileUtils;
-	import org.apache.commons.io.FileUtils;
+	
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.OutputType;
 	import org.openqa.selenium.TakesScreenshot;
-	import org.openqa.selenium.TakesScreenshot;
+	
 	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.WebElement;
 	import org.openqa.selenium.firefox.FirefoxDriver;
